8760052b25d715aa8471cbb884b4f5fefe07b470
Rodrigo <rodrigomartins.agro@gmail.com>
2022-08-16 11:53:11 -0300

    Exercicios

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte2.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte2.js
@@ -12,3 +12,19 @@ const print = factorial(4);
 console.log(`Esse é o fatorial ${print}`);
 
 // Exercicio 2
+const longestWord = (text) => text.split(' ').sort((wordA, wordB) => wordB.length - wordA.length)[0];
+console.log(longestWord('Antonio foi ao banheiro e não sabemos o que aconteceu'));
+
+const longestWord2 = (text) => {
+    const wordArray = text.split(' ');
+    let maxLength = 0;
+    let result = '';
+    for (const word of wordArray) {
+        if (word.length > maxLength) {
+            maxLength = word.length;
+            result = word;
+        }
+    }
+    return result;
+}
+console.log(longestWord2('Antonio foi ao banheiro e não sabemos o que aconteceu'));
